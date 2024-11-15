d854149e92c86426b053f2219f96757371916194
ReginaTR <regininhar@hotmail.com>
2024-11-15 16:17:59 -0300

    Configurando o status em Task

--- web/app/models/task.rb
+++ web/app/models/task.rb
@@ -1,6 +1,5 @@
 class Task < ApplicationRecord
-    enum status: { pendente: 0, em_progresso: 1, concluida: 2, falha: 3 }
-
+    
     validates :title, presence: true
     validates :url, presence: true
 end
